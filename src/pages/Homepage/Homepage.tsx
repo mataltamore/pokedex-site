e09ad61a8dab9efee7d1d32574de38ec9f0dc7f3
Matteo Altamore <maltamore@deloitte.it>
2022-04-30 18:01:30 +0200

    pokemon api call add

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Homepage.module.scss";
 
-const Homepage = () => {
-  return <div>Homepage</div>;
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+const BASE_URL_PKMN_INFO = "https://pokeapi.co/api/v2/pokemon/";
+
+type CompleteData = {
+  id: number;
+  name: string;
+  types: Array<{ name: string }>;
+  sprite: string;
+};
+
+type DataMon = {
+  id: number;
+  sprites: { other: { "official-artwork": { front_default: string } } };
+  types: Array<{ slot: number; type: { name: string; url: string } }>;
+};
+
+type Result = { name: string; url: string };
+
+const Homepage = ({
+  pokemonList,
+}: InferGetServerSidePropsType<GetServerSideProps>) => {
+  const [data, setData] = useState<Array<CompleteData>>([]);
+
+  useEffect(() => {
+    pokemonList.results.forEach(async (mon: Result) => {
+      const response = await fetch(BASE_URL_PKMN_INFO + mon.name);
+      const data: DataMon = await response.json();
+
+      const newPokemon: CompleteData = {
+        id: data.id,
+        name: mon.name,
+        types: data.types.map((item) => {
+          return { name: item.type.name };
+        }),
+        sprite: data.sprites.other["official-artwork"].front_default,
+      };
+
+      setData((prev) => [...prev, newPokemon]);
+    });
+  }, [pokemonList]);
+
+  return (
+    <>
+      {data.map((mon: CompleteData) => {
+        return <div key={mon.id}>{JSON.stringify(mon)}</div>;
+      })}
+    </>
+  );
 };
 
 export default Homepage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,33 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import HomePage from "./Homepage/Homepage";
 
-const Home: NextPage = () => {
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+const URL_PKMN_LIST =
+  "https://pokeapi.co/api/v2/pokemon-species/?offset=151&limit=3";
+
+type DataList = {
+  count: string;
+  next: string;
+  previous: string;
+  results: Array<Result>;
+};
+
+type Result = { name: string; url: string };
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch(URL_PKMN_LIST);
+  const pokemonList: DataList = await response.json();
+
+  return {
+    props: {
+      pokemonList,
+    },
+  };
+};
+
+const Home: NextPage = ({
+  pokemonList,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <Head>
@@ -10,7 +36,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Pokemon fanmade site" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomePage />
+      <HomePage pokemonList={pokemonList} />
     </>
   );
 };
