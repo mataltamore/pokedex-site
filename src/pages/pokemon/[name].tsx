8d25aba598730e185a903931d78e8fd9ca63abe4
Matteo Altamore <maltamore@deloitte.it>
2022-05-03 21:04:35 +0200

    detail page useEffect to getServerSideProps

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -4,29 +4,31 @@ import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 import DetailPage from "../../components/DetailPage/DetailPage";
 
-const Pokemon: NextPage = () => {
-  const router = useRouter();
-  const { id, name } = router.query;
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id, name } = query;
 
   const BASE_URL = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const titleName =
     name && typeof name === "string"
       ? name.charAt(0).toUpperCase() + name.slice(1)
       : "Simple Pokedex";
-  const [pokemonData, setPokemonData] = useState({});
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(BASE_URL);
-      const data = await response.json();
-      setPokemonData(data);
-    }
-    fetchData();
-  }, [BASE_URL]);
+  const response = await fetch(BASE_URL);
+  const pokemonData = await response.json();
 
+  return {
+    props: { pokemonData, titleName },
+  };
+};
+
+const Pokemon: NextPage = ({
+  pokemonData,
+  titleName,
+}: InferGetServerSidePropsType<GetServerSideProps>) => {
   return (
     <>
       <Head>
